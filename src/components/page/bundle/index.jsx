999e962ec8b726036488cc32390cf89a62c97046
Safaa-Ebrahim <ebrasafa@gmail.com>
2026-07-13 22:03:26 +0300

    Add Bundle component to manage product selection and review, integrating data and hooks for enhanced functionality.

---
+++ src/components/page/bundle/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import data from '../../../data/products.json'
+import { useBundle } from '../../../hooks/useBundle'
+import Builder from './builder'
+import ReviewPanel from './review'
+
+const Bundle = () => {
+    const bundle = useBundle(data)
+
+    return (
+        <div className="min-h-svh py-8 px-4">
+            <div className="auto grid grid-cols-1 lg:grid-cols-[1fr_380px] xl:grid-cols-1 gap-6 items-start ">
+                <Builder data={data} bundle={bundle} />
+                <ReviewPanel data={data} bundle={bundle} />
+            </div>
+        </div>
+    )
+}
+
+export default Bundle
