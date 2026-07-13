102051c254f75f95f67440d24da907c22d9fe6b2
Safaa-Ebrahim <ebrasafa@gmail.com>
2026-07-13 22:03:12 +0300

    Add stepIcons.js to define a set of icons for different steps in the application, enhancing visual representation and user experience.

---
+++ src/config/stepIcons.js
@@ -0,0 +1,10 @@
+import { FiCamera, FiShield } from 'react-icons/fi'
+import { PiWifiHighBold } from 'react-icons/pi'
+import { MdOutlineAddModerator } from 'react-icons/md'
+
+export const STEP_ICONS = {
+  camera: FiCamera,
+  shield: FiShield,
+  sensor: PiWifiHighBold,
+  protection: MdOutlineAddModerator,
+}
