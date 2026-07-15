d3832585649ffb19d79ec176c96a21d55e123938
Safaa-Ebrahim <ebrasafa@gmail.com>
2026-07-15 16:40:15 +0300

    Fix grid class in BundleContent component: update class from 'auto grid' to 'grid' for improved layout consistency.

--- src/components/bundle/BundleContent.jsx
+++ src/components/bundle/BundleContent.jsx
@@ -8,7 +8,7 @@ export default function BundleContent({ data }) {
   return (
     <div className="min-h-svh py-8 px-4 xl:px-10">
       <p className='block lg:hidden font-bold text-3xl text-text-secondary text-center mb-3'>Let's get started!</p>
-      <div className="auto grid grid-cols-1 lg:grid-cols-[1fr_380px] xl:grid-cols-1 gap-6 items-start ">
+      <div className="grid grid-cols-1 lg:grid-cols-[1fr_380px] xl:grid-cols-1 gap-6 items-start">
         <Builder data={data} bundle={bundle} />
         <ReviewPanel data={data} bundle={bundle} />
       </div>
